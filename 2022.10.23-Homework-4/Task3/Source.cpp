610813d81d5b85dbd82a7bc561779d99a7c7b4ee
108202 <dalginichev1@bk.ru>
2022-10-30 20:44:44 +0300

    Homework

---
+++ 2022.10.23-Homework-4/Task3/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int i = 0;
+	int a = 1;
+	int b = 1;
+
+	std::cin >> n;
+
+	for (i = 1; i <= n; i++)
+	{
+		std::cout << a << " ";
+
+		b += 1;
+		if (b > a)
+		{
+			a += 1;
+			b = 1;
+		}
+	}
+	return EXIT_SUCCESS;
+}
